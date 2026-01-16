976b65023ece7beebab9630bdd941c07ed35b390
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-01-16 22:34:34 +0530

    First React-Native

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text 
+        style={{
+          fontSize: 30,
+        }}
+      >Hello World from First React-Native with Expo</Text>
+    </View>
+  );
+}
